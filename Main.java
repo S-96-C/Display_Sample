9d69eae9563bd127257b2056dd4a6ee82d4de11d
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-07-30 18:27:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args){
+		/*System.out.println("Hello World!");
+		System.out.println("It's been nice knowing you.");
+		System.out.println("Goodbye world!");*/
+		
+		System.out.println("Hello World! \nIt's been nice knowing you. \nGoodbye world! Have a nice day");
+		//Single Line Comment
+		/*Multiple Line Comment*/
+	}
+	
+}
